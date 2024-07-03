0d18bdf4caf191fce7e4ad9f0ab82d7b7fe5122f
kafeindev <kafein40@gmail.com>
2024-07-03 21:38:43 +0300

    feat: add ```actions``` file

---
+++ lib/actions.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import fs from "fs";
+import { readdir } from "fs/promises";
+
+import { serialize } from "next-mdx-remote/serialize";
+
+import { Post } from "./types";
+
+export async function getPosts(): Promise<Post[]> {
+  const slugs = await readdir("./resources/posts/", { withFileTypes: true });
+
+  const posts = await Promise.all(
+    slugs.map(async ({ name }) => {
+      const post = await getPost(name.replace(".mdx", ""));
+      return post;
+    })
+  );
+
+  posts.sort((a, b) => +new Date(b.publishDate) - +new Date(a.publishDate));
+
+  return posts;
+}
+
+export async function getPost(slug: string): Promise<Post> {
+  const { metadata } = await import(`../resources/posts/${slug}.mdx`);
+
+  const fileContent = fs.readFileSync(`./resources/posts/${slug}.mdx`, "utf-8");
+  const serializedFile = await serialize(fileContent);
+
+  return {
+    source: serializedFile,
+    slug: slug,
+    ...metadata,
+  };
+}
