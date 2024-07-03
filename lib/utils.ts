ff0624e0095d2cf011c07e1b06f250910d66f375
kafeindev <kafein40@gmail.com>
2024-07-03 21:39:16 +0300

    feat: add ```formatDate``` function

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,11 @@ export function cn(...inputs: ClassValue[]) {
 export function isExternalLink(url: string) {
   return url.startsWith("http");
 }
+
+export function formatDate(date: Date) {
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
